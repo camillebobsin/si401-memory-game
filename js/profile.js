4f408aedc3c709deb8f2bcef678fb4081355843b
camillebobsin <camillebobsin.cb@gmail.com>
2022-11-10 22:48:22 -0300

    Style: changed the popup style

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
-document.getElementById("tiger").addEventListener("click", closePopup);
 document.getElementById("sheep").addEventListener("click", closePopup);
-document.getElementById("whale").addEventListener("click", closePopup);
+document.getElementById("bird").addEventListener("click", closePopup);
+document.getElementById("otter").addEventListener("click", closePopup);
+document.getElementById("tiger").addEventListener("click", closePopup);
 document.getElementById("panda").addEventListener("click", closePopup);
 
 function openPopup() {
@@ -8,17 +9,32 @@ function openPopup() {
     popup.classList.add("open-popup");
 }
 
+let index = 0;
 function closePopup() {
-    let index = 0;
     let popup = document.getElementById("popup");
     popup.classList.remove("open-popup");
-    if(this.id == "tiger") 
-        index = 10;
     if(this.id == "sheep") 
         index = 0;
-    if(this.id == "whale") 
-        index = 7;
+    if(this.id == "bird") 
+        index = 23;
+    if(this.id == "otter") 
+        index = 14;
+    if(this.id == "tiger") 
+        index = 10;
     if(this.id == "panda") 
         index = 20;
     document.getElementById("act").style.backgroundImage = "url('assets/cards/card_" + index + ".jpg')"
+}
+
+let act = document.getElementById("act");
+act.addEventListener("mouseover", hoverOn);
+act.addEventListener('mouseout', hoverOff);
+
+function hoverOn() {
+    console.log(index)
+    act.style.backgroundImage = "url('../assets/pencil-icon.png')";
+}
+
+function hoverOff() {
+    act.style.backgroundImage = "url('assets/cards/card_" + index + ".jpg')";
 }
